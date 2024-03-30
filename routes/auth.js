1d47decf0e532c85e217e58b8fa7763c68df8651
Kurd Wolf <124709651+Jahanpour7@users.noreply.github.com>
2024-03-30 08:13:19 +0100

    auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-    res.sendFile('index.html', { root: 'views' });
+// User login
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+// User signup
+router.post('/signup', (req, res) => {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email, password });
+    newUser.save()
+        .then(user => {
+            req.login(user, err => {
+                if (err) return next(err);
+                res.redirect('/');
+            });
+        })
+        .catch(err => console.error('Error signing up user:', err));
+});
+
+// User logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
